fe3619b81080e426f8cca13056295444091e2778
Ovvugt <oscar.vugt@gmail.com>
2021-09-10 15:04:08 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import logo from './logo.png'
+
+function App() {
+  return (
+    <header>
+      <div className="navbar">
+        <img src={logo} alt="Logo" className="logo"></img>
+      </div>
+    </header>
+  );
+}
+
+export default App;
