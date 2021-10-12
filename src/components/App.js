9985efce7c0593ca3ffff28bf75597d3309c592d
InnocentNyuu <sharoek@live.nl>
2021-10-12 19:09:03 +0200

    [Update] image height modified. Added class logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import NavBar from './NavBar'
 import Main from './Main'
+import Logo from './logo';
 
 function App() {
   return (
     <div className="App">
       <Main />
       <NavBar />
+      <Logo />
     </div>
   );
 }
