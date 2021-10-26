7aed100602fd2c7d9e568f1096e60b448ecb8f4a
Jue Jun Ma <1014477@hr.nl>
2021-10-26 13:59:01 +0200

    .

--- src/pages/Information.js
+++ src/pages/Information.js
@@ -7,8 +7,16 @@ import video1 from "../../src/assets/videos/Wat gebeurt er met mijn bloed_.mp4";
 class Information extends React.Component {
     render() {
         return (
-            <Collapsible src={syringe} video={video1} classname="Syringe"/>      
-
+            <div class="container">
+                <div class="title">
+                    <h1>Information</h1>
+                </div>
+                <b>Let op: In verband met het Coronavirus werken we voor bloedafname met geplande afspraken. Dit is om te voorkomen dat er te veel patiënten tegelijk in de wachtruimte zijn. U kunt hier online een afspraak maken voor bloedafname.</b>
+                <h2>Voorbereiding</h2>
+                <p style={{margin: 0}} class="paragraf">Wat je moet meenemen:</p>
+                <span class="paragraf">- Aanvraagformulier<br/>- Verzekeringspas<br/>- Identiteitsbewijs </span>
+                <Collapsible src={syringe} video={video1} classname="Syringe"/>      
+            </div>
         )
        
     }
