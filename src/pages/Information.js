fc4c822821293225a15d6e9bfadfca232a8a3cb9
Jue Jun Ma <1014477@hr.nl>
2021-11-16 14:14:38 +0100

    Text style and navbar adjusted

--- src/pages/Information.js
+++ src/pages/Information.js
@@ -11,7 +11,7 @@ class Information extends React.Component {
         return (
             
             <div class="container">
-                <div className="grid">
+                {/* <div className="grid">
                     <div className="grid1">
                         <div className="textAnimation">
                             <p class="paragraf">Let op: </p>
@@ -21,20 +21,21 @@ class Information extends React.Component {
                             </p>
                         </div>
                     </div>
-            </div>
+            </div> */}
 
-
-
-
+                <h1>Informatie</h1>
                 <h2>Wat is het doel van bloedprikken?</h2>
                 <p>Door bloed prikken krijg je informatie over je lichaam. Met het informatie die we krijgen kunnen we kijken of je organen gezond zijn en goed functioneren. </p>
                 <h2>Voorbereiding</h2>
-                <p style={{margin: 0}} class="paragraf">Wat je moet meenemen naar het bloedprikken:</p>
-                <span class="paragraf">- Aanvraagformulier<br/>- Verzekeringspas<br/>- Identiteitsbewijs </span>
+                <p class="paragraf">Wat je moet meenemen naar het bloedprikken:</p>
+                <ul class="paragraf">
+                    <li>Aanvraagformulier</li>
+                    <li>Verzekeringspas</li>
+                    <li>Identiteitsbewijs</li>
+                </ul>
                 <h2>Verloop onderzoek</h2>
                 <p>
                     De bloedafname vindt plaats terwijl u zit.
-                     Er word één of meerdere buisjes bloed afgenomen door een medewerker die een prik aan de binnenkant van uw elleboog 
                      Om één of meerdere buisjes bloed af te kunnen nemen prikt de medewerker bloedafname aan de binnenkant van uw elleboog in een ader. 
                      Hiervoor krijgt u een zogenaamde stuwband om uw arm. 
                      Tijdens de bloedafname geeft de medewerker aanwijzingen om het onderzoek zo snel en goed mogelijk te laten verlopen. 
