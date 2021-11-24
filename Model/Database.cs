ac5bcf1d1d6234b6955e19fd9fed6799466dd9d8
Sharoek Mahboeb <1018492@hr.nl>
2021-11-24 12:09:49 +0100

    Add Database branch

---
+++ Model/Database.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Npgsql;
+
+
+    
+var cs = "Host=145.24.222.54;Username=postgres;Password=s$cret;Database=testdb";
+
+using var con = new NpgsqlConnection(cs);
+con.Open();
+
+var sql = "SELECT version()";
+
+using var cmd = new NpgsqlCommand(sql, con);
+
+var version = cmd.ExecuteScalar().ToString();
+Console.WriteLine($"PostgreSQL version: {version}");
+    
