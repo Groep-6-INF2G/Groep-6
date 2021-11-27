f35b2e47336ff02d068ab1c0be0137e3974c2ec8
Sharoek <sharoek@live.nl>
2021-11-27 17:10:18 +0100

    [Update]
    * Database is connected with C#
    * Method added for displaying all data
    * 2 Methods commented for creating table and adding data
    
    - The method is for now being run after the program is closed

--- Model/Database.cs
+++ Model/Database.cs
@@ -1,17 +1,81 @@
 ﻿using System;
+using System.IO;
 using Npgsql;
 
+namespace Database
+{
+    public class NPGSQL
+    {
+        public NpgsqlConnection databaseConnection;
 
-    
-var cs = "Host=145.24.222.54;Username=postgres;Password=s$cret;Database=testdb";
+        public NPGSQL()
+        {
+            databaseConnection = new NpgsqlConnection("Host=145.24.222.54; Username=postgres;Password=team6;Database=postgres;Port=8011;IncludeErrorDetail=true");
+        }
 
-using var con = new NpgsqlConnection(cs);
-con.Open();
+        public void selectAllSQL()
+        {
 
-var sql = "SELECT version()";
+            using var connection = databaseConnection;
+            connection.Open();
+            var sql = "SELECT * FROM locations"; ;
 
-using var cmd = new NpgsqlCommand(sql, con);
+            using var cmd = new NpgsqlCommand(sql, connection);
+            using NpgsqlDataReader rdr = cmd.ExecuteReader();
 
-var version = cmd.ExecuteScalar().ToString();
-Console.WriteLine($"PostgreSQL version: {version}");
+            while (rdr.Read())
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4} {5}", rdr.GetString(0), rdr.GetString(1),
+                        rdr.GetString(2), rdr.GetString(3), rdr.GetString(4), rdr.GetString(5));
+            }
+
+            //createTable(connection);
+            //addLocations(connection);
+        }
+
+
+        // 2 Methods for creating a table and adding data if needed
+        /*
+        public void createTable(NpgsqlConnection newConnection)
+        {
+            NpgsqlCommand createLocationTable = new NpgsqlCommand(
+           "CREATE TABLE IF NOT EXISTS locations" +
+           "(ID SERIAL PRIMARY KEY," +
+           "location TEXT," +
+           "locationDescription TEXT," +
+           "address TEXT," +
+           "postalCode TEXT," +
+           "locationTime TEXT," +
+           "appointmentDescription TEXT)"
+           , newConnection);
+            createLocationTable.ExecuteNonQuery();
+            createLocationTable.Connection = newConnection;
+        }
+
+
+        public void addLocations(NpgsqlConnection newConnection)
+        {
+           string line = "";
+           using (StreamReader file = new StreamReader(@".\Controllers\priklocatie_sanitized.sql"))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] fields = line.Split(',');
+                    NpgsqlCommand cmd = new NpgsqlCommand("INSERT INTO locations(location, locationDescription, address,postalCode, locationTime, appointmentDescription) VALUES (@location, @locationDescription, @address, @postalCode, @locationTime, @appointmentDescription)", newConnection);
+                    cmd.Parameters.AddWithValue("@location", fields[0].ToString());
+                    cmd.Parameters.AddWithValue("@locationDescription", fields[1].ToString());
+                    cmd.Parameters.AddWithValue("@address", fields[2].ToString());
+                    cmd.Parameters.AddWithValue("@postalCode", fields[3].ToString());
+                    cmd.Parameters.AddWithValue("@locationTime", fields[4].ToString());
+                    cmd.Parameters.AddWithValue("@appointmentDescription", fields[5].ToString());
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }*/
+
+    }
+}
+
+
+
     
