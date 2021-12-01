d3bcd38be3c555472e96573923ed67ee53127476
Jue Jun Ma <1014477@hr.nl>
2021-12-01 01:00:13 +0100

    Tinymce installed but not working yet

---
+++ ClientApp/src/components/tinymce-init.js
@@ -0,0 +1,30 @@
+﻿import React, { Component } from "react";
+import tinymce from "tinymce";
+import "tinymce/themes/silver";
+import "tinymce/plugins/wordcount";
+import "tinymce/plugins/table";
+
+class TinyEditorComponent extends Component {
+    componentDidMount() {
+        tinymce.init({
+            selector: `#${this.props.id}`,
+            skin_url: `${process.env.PUBLIC_URL}/skins/ui/oxide`,
+            plugins: 'wordcount table',
+            inline: true,
+            setup: editor => {
+                this.setState({ editor });
+                editor.on('keyup change', () => {
+                    const content = editor.getContent();
+                    this.props.onEditorChange(content);
+                });
+            }
+        });
+    }
+    componentWillUnmount() {
+        tinymce.remove(this.state.editor);
+    }
+    render() {
+        return (<textarea id="{this.props.id}" />);
+    }
+}
+export default TinyEditorComponent;
--- ClientApp/src/pages/HomePage.js
+++ ClientApp/src/pages/HomePage.js
@@ -1,22 +1,16 @@
+/// <reference path="../../node_modules/tinymce/tinymce.min.js" />
 import React from "react"
 import '../styles/homepage.css';
-import StarWhiteLogo from "../assets/images/Star-shlWit.png";
+import "jquery"
+import { Editor } from '@tinymce/tinymce-react';
 class HomePage extends React.Component {
     render() {
         return (
             <>
-                <div class="containerHome">
-                    <div class="text">
-                        <h1>Welkom op de Star-Shl web-app</h1>
-                        <p>Op deze website kan je informatie vinden over bloedprikken en de dichtsbijzijnde locaties van Star-Shl </p>
-                    </div>
-                </div>
-                <div class="inner-container">
-                    <p style={{paddingBottom: "15px" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt dolor sed diam ullamcorper aliquet. Phasellus bibendum enim quis commodo commodo. Nullam facilisis bibendum tincidunt. Quisque iaculis magna id velit lobortis pretium. Cras at mi eu mauris luctus porta dapibus at erat. Sed vehicula tincidunt risus, vel ornare purus. Nam egestas lacus sed velit volutpat, non maximus enim dignissim. Vestibulum quis risus finibus, condimentum arcu a, accumsan erat. Nam lacinia neque nec urna molestie, quis suscipit quam eleifend. Aenean nisl lacus, mollis vel nibh quis, sollicitudin finibus ligula.</p>
-                    <p style={{paddingBottom: "15px"}}>In id quam et turpis cursus feugiat. Nam dapibus fringilla massa, et volutpat lacus ultricies sed. Nam ullamcorper gravida nisl, sit amet gravida ligula interdum ac. Nunc quis lectus commodo, mattis quam at, malesuada arcu. In eu magna odio. Donec id mi vel mauris auctor fermentum vitae a odio. Duis eu vulputate neque, consequat condimentum sapien. In eu lacus imperdiet, pharetra erat vel, euismod diam.</p>
-                </div>
+                <Editor initialValue="<p>bruh.</p>" init={{ height: 500, inline: true }}/>
+                <script src="../../node_modules/tinymce/tinymce.min.js"></script>
             </>
-        )     
+        )
     }
 }
 
