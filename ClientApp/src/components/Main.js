1addc26e697b4dc6e26ce1299d5bcf1164f11849
Jue Jun Ma <jjma2003@gmail.com>
2021-12-03 13:24:57 +0100

    You can edit the HomePage now but the changes aren't saved yet

--- ClientApp/src/components/Main.js
+++ ClientApp/src/components/Main.js
@@ -5,6 +5,7 @@ import Home from '../pages/HomePage';
 import Information from '../pages/Information';
 import Locations from '../pages/Locations';
 import Login from '../pages/Login'
+import HomeEdit from '../pages/HomePage-Editor'
 
 const Main = () => {
   return (
@@ -14,6 +15,8 @@ const Main = () => {
         <Route exact path='/Informatie' component={Information}></Route>
         <Route exact path='/Locaties' component={Locations}></Route>
         <Route exact path='/Inloggen' component={Login}></Route>
+        <Route exact path='/HomeEditor' component={HomeEdit}></Route>
+
       </Switch>
     </div>
   );
