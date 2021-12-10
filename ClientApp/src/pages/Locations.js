a01b88db7d005408389c70b9fe253705fc4323e7
Dennis Houwen <18077943@student.hhs.nl>
2021-12-10 13:23:59 +0100

    Adds search box to location page (ft. DraxNoid)

--- ClientApp/src/pages/Locations.js
+++ ClientApp/src/pages/Locations.js
@@ -1,17 +1,38 @@
 import React from "react";
-import '../styles/information.css';
+import '../styles/locations.css';
 
 class Locations extends React.Component {
+    constructor() {
+        super()
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.state = {
+            postcode: ''
+        }
+
+    }
+
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    async handleSubmit(e) {
+        e.preventDefault();
+        console.log(e);
+    }
     render() {
         return (
-            <div class= "container">
-                <h1>
-                    Locaties
-                </h1>
-                
-                <p style={{marginTop:"12px"}}>
-                    Hier komt binnenkort een zoekfunctionaliteit om de priklocaties van Star-SHL bij U in de buurt te vinden terecht. Tot dan!
-                </p>
+            <div class="location-container">
+                <div class ="search-box">
+                    <p class="title">Zoek dichtsbijzijnde locatie</p>
+                    <div class="search-field">
+                        <form onSubmit={this.handleSubmit}>
+                            <input type="postcode" name="postcode" placeholder="Postcode" required onChange={this.handleChange} />
+                            <button onSubmit={this.handleSubmit}>Zoek</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         )
     }
