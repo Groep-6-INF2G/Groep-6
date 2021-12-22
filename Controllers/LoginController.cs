6fe56a525a7f28c103a9682467013335e5c045bf
Sharoek <sharoek@live.nl>
2021-12-22 01:00:49 +0100

    Added check and notification if email is correct and password incorrect

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -28,10 +28,11 @@ namespace Git_clone.Controllers
                 var jsonUser = JsonSerializer.Serialize(user);
                 return Ok(jsonUser);
             }
-            else
-            {
-                return StatusCode(401);
-            }
+            else if  (users.FirstOrDefault(x => x.Email == loginInfo.Email && x.Password != loginInfo.Password) is User user1)
+                {
+                    return StatusCode(404);
+                }
+            return StatusCode(401);
         }
     }
 }
