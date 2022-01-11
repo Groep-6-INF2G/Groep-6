2db8f779fae65fd633849ee90dccc64e54b03cde
Sharoek <sharoek@live.nl>
2022-01-11 22:28:39 +0100

    QR code is shown from backend to frontend. Scanning the QR works also. Still needs to implement the verification.

--- ClientApp/src/pages/Verification.js
+++ ClientApp/src/pages/Verification.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react'
 import '../styles/VerificationStyle.css'
+import GetQR from '../components/fetchImage'
+
 
 class Verification extends React.Component {
     render() {
         return (
-            <>
                 <div id='container' class='containerVerify'>
                     <div class='text'>
                         <h1>Verificatie</h1>
-                        <div class='inputfields'>
-                            <form onSubmit={this.handleSubmit}>
-                                <input type="Code" name="Code" placeholder="6-Cijferig Code" required onChange={this.handleChange} />
-                                <button onSubmit={this.handleSubmit}>Log in</button>
-                            </form>
-                        </div>
-                        <p>Voer de 6-cijferige code in. Is de code niet verstuurd? klik dan op onderstaande knop.</p>
-                        <div class='inputfields'>
-                            <button onSubmit={this.handleSubmit}>Resend</button>
-                        </div>
                     </div>
+                    <p>Scan de QR code met een custom QR app:</p>
+                <GetQR />
+
                 </div>
-            </>
         )
     }
 }
---
+++ Controllers/QRController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TwoFactorAuthNet;
+using TwoFactorAuthNetSkiaSharpQrProvider;
+
+namespace Git_clone.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QRController : Controller
+    {
+        [HttpGet]
+        public string createQRSecretController()
+        {
+            // Though the default is an 80 bits secret (for backwards compatibility reasons) we 
+            // recommend creating 160+ bits secrets (see RFC 4226 - Algorithm Requirements)
+            var myTwoFactorAuth = new TwoFactorAuth("groep6", qrcodeprovider: new SkiaSharpQrCodeProvider());
+            var secret = myTwoFactorAuth.CreateSecret(80);
+          
+            return myTwoFactorAuth.GetQrCodeImageAsDataUri("secretcode", secret); 
+        }
+
+       
+    }
+}
